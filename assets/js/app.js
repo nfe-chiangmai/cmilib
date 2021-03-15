83bd013bcd3a5adb86879c79f9ef8a377e397385
James Duncan <james@digiserve.org>
2021-03-15 15:59:24 +0700

    Update path to service worker.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("sw.js")
+        .register("/princessLibraryProject/assets/js/sw.js")
         .then(() => console.log("registered service worker!"));
 }
