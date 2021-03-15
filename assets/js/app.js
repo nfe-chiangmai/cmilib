e16986fa980503ef3c7f879fa34dc6cc00ce3e97
James Duncan <james@digiserve.org>
2021-03-15 16:09:40 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+// In order to provide an offline experience we need to register service worker to fetch and store assets
+// Our service worker is located at "/princessLibraryProject/assets/js/sw.js" go there to more details 
+
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
         .register("/princessLibraryProject/assets/js/sw.js")
